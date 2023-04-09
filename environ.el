89e5d5742baad5b1fd2905fb15724a46d173fbd6
Chris Clark <cfclrk@gmail.com>
2023-04-09 11:22:06 -0400

    Add keyword

--- environ.el
+++ environ.el
@@ -4,6 +4,7 @@
 ;; Version: 0.0.1
 ;; Package-Requires: ((emacs "24.1") (dash "2.17.0") (f "0.20.0") (s "1.12.0"))
 ;; URL: https://github.com/cfclrk/environ
+;; Keywords: tools
 
 ;; This file is NOT part of GNU Emacs.
 
